7e320b329a08772f2164bb32652af8c6df16e55f
Krish0257 <143323533+Krish0257@users.noreply.github.com>
2024-06-07 16:06:33 +0530

    commit

--- simple-python-app/scripts/start_container.sh
+++ simple-python-app/scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
-
+echo "pulling docker image"
+docker pull krishna257/simple-python-flask-app
 # Run the Docker image as a container
-echo
+echo "running the docker image"
+ docker run -d -p 5000:5000 krishna257/simple-python-flask-app
